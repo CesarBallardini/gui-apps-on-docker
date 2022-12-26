d369ba3bb97ec3c27c768d7068cf88c9099f5849
Cesar Ballardini <cesar.ballardini@gmail.com>
2022-12-26 00:11:14 -0300

    no-proxy configs

---
+++ linux-container-on-win-vm/provision/setup0.ps1
@@ -0,0 +1,15 @@
+#Set-PSDebug -Trace 1
+
+# set RDP
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# enable WinRM: already enabled in box
+#powershell -Command Enable-PSRemoting -Force
+
+Set-TimeZone "Coordinated Universal Time" -Verbose
+Disable-BingSearch
+Set-TaskbarOptions -Dock Bottom
+Update-Help
+Install-WindowsUpdate
+
