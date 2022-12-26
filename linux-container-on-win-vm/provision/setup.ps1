b680912ab44c68eb03c4670e80eb4869f0d51be4
Cesar Ballardini <cesar.ballardini@gmail.com>
2022-12-26 15:03:08 -0300

    setup0 working with proxies

--- linux-container-on-win-vm/provision/setup.ps1
+++ linux-container-on-win-vm/provision/setup.ps1
@@ -14,6 +14,42 @@ Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtec
 choco feature enable -n=allowGlobalConfirmation
 choco feature enable -n=useRememberedArgumentsForUpgrades
 
+# Useful apps
+choco install microsoft-windows-terminal
+#choco install vlc
+#choco install keepass
+
+choco install 7zip
+#choco install adobereader
+#choco install barrier
+#choco install falkon
+#choco install fbreader
+#choco install libreoffice-fresh
+choco install lockhunter
+choco install mc -ia /TASKS="desktopicon,modifypath"
+#choco install obs-studio.install
+choco install okular
+#choco install openconnect-gui
+#choco install skype
+#choco install slack
+#choco install sysinternals
+choco install vim
+choco install winrar
+choco install zip
+#choco install zoom
+
+# Git
+choco install git -params '"/GitAndUnixToolsOnPath /NoAutoCrlf"'
+choco install git-credential-manager-for-windows
+
+# must be installed as vagrant user
+#git config --global core.autocrlf  input
+#git config --global core.symlinks true
+#git config --global user.name "Cesar Ballardini"
+#git config --global user.email cesar.ballardini@gmail.com
+#git config --global core.editor vim
+#git config --global credential.helper  manager-core
+
 ##
 # WSL2
 #
@@ -52,6 +88,19 @@ dism.exe /Online  /Get-FeatureInfo:Microsoft-Windows-Subsystem-Linux
 
 dism.exe /Online /Enable-Feature /FeatureName:VirtualMachinePlatform /All
 bcdedit /set hypervisorlaunchtype auto
+
+# Python
+choco install pyenv-win
+
+#Adding PYENV, PYENV_HOME and PYENV_ROOT to your Environment Variables
+[System.Environment]::SetEnvironmentVariable('PYENV',$env:USERPROFILE + "\.pyenv\pyenv-win\","User")
+
+[System.Environment]::SetEnvironmentVariable('PYENV_ROOT',$env:USERPROFILE + "\.pyenv\pyenv-win\","User")
+
+[System.Environment]::SetEnvironmentVariable('PYENV_HOME',$env:USERPROFILE + "\.pyenv\pyenv-win\","User")
+
+#Now adding the following paths to your USER PATH variable in order to access the pyenv command
+[System.Environment]::SetEnvironmentVariable('path', $env:USERPROFILE + "\.pyenv\pyenv-win\bin;" + $env:USERPROFILE + "\.pyenv\pyenv-win\shims;" + [System.Environment]::GetEnvironmentVariable('path', "User"),"User")
 
 Set-ExecutionPolicy -Scope CurrentUser unrestricted
 
