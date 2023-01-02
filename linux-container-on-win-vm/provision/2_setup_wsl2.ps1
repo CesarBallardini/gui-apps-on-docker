e3c82bec4cd497c9db69f13e04f4344f69a25315
Cesar Ballardini <cesar.ballardini@gmail.com>
2023-01-02 13:56:25 -0300

    added console messages, windows updates

--- linux-container-on-win-vm/provision/2_setup_wsl2.ps1
+++ linux-container-on-win-vm/provision/2_setup_wsl2.ps1
@@ -1,22 +1,37 @@
 # Boxstarter setup script
 
-# update WSL2
+Write-Host "WSL: update WSL2"
 wsl --update
+
+Write-Host "WSL2: shutdown WSL2"
 wsl --shutdown
 
-# Step 4: Set WSL2 as the default version
+Write-Host "WSL2: set 2 as the default version"
 wsl --set-default-version 2
-# Step 6: Install your preferred Linux distribution
 
+Write-Host "Install your preferred Linux distribution"
+
+Write-Host "List the available Linux distributions"
 wsl --list --verbose
-wsl --install
+
+Write-Host "Install Ubuntu Linux distribution"
+#wsl --install
+wsl --install -d Ubuntu
 
 #Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 dism.exe /Online  /Get-FeatureInfo:VirtualMachinePlatform
 dism.exe /Online  /Get-FeatureInfo:HypervisorPlatform
 dism.exe /Online  /Get-FeatureInfo:Microsoft-Windows-Subsystem-Linux
 
+Write-Host "System Type is:"
 systeminfo | find '"System Type"'
 
-Set-ExecutionPolicy -Scope CurrentUser unrestricted
+$expol = Get-ExecutionPolicy -Scope CurrentUser
+if ($expol -ne "Unrestricted") {
+  Write-Host "changing to unrestricted the execution policy"
+  Set-ExecutionPolicy -Scope CurrentUser unrestricted
+  #Set-ExecutionPolicy -Scope MachinePolicy Unrestricted
+} else {
+  Write-Host "execution policy is already unrestricted"
+}
 
